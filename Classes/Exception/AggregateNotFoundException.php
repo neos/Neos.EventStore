831accc409aea2835b46070ce2e612b872f0651f
Dominique Feyer <dfeyer@ttree.ch>
2016-08-27 16:17:10 +0200

    TASK: Add AggregateNotFoundException

---
+++ Classes/Exception/AggregateNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+namespace Ttree\EventStore\Exception;
+
+/*
+ * This file is part of the Ttree.Cqrs package.
+ *
+ * (c) Hand crafted with love in each details by medialib.tv
+ */
+
+use Ttree\Cqrs\Exception;
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * AggregateNotFoundException
+ */
+class AggregateNotFoundException extends Exception
+{
+}
